c70d6c24cb52142bb5d2ec3774e57e8fc5230692
Qiao Yang Han <qyhwork@gmail.com>
2023-01-15 20:14:45 -0500

    Worked on Regular Expression Matching

--- longestCommonSubsequence.js
+++ longestCommonSubsequence.js
@@ -7,9 +7,17 @@ const longestCommonSubsequence = (text1, text2) => {
     
     for (let i = 0; i <= m; i++) {
         for (let j = 0; j <= n; j++) {
-            if(text1[i - 1] === text2[j - 2]) {
+            if (i === 0 || j === 0) {
+                dp[i][j] = 0;
+            }
+            else if(text1[i - 1] === text2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
+
+    return dp[m][n];
 }
