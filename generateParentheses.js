88802cbe66e7e04243775689cde5bf6647b3b1f4
Qiao Yang Han <qyhwork@gmail.com>
2023-01-17 10:37:32 -0500

    Worked on Generating Parenthesis

--- generateParentheses.js
+++ generateParentheses.js
@@ -51,9 +51,20 @@ const genBalancedDP = (n) => {
 }
 
 const generateParenthesis = (n) => {
-
+    const ans = [];
+    backtrack(ans, "", 0, 0, n);
+    return ans;
 }
 
-const backtrack = (n) => {
-    
+const backtrack = (ans, cur, open, close, max) => {
+    if (cur.length === max * 2) {
+        ans.push(cur);
+        return;
+    }
+    if (open < max) {
+        backtrack(ans, cur + "(", open + 1, close, max);
+    }
+    if (close < open) {
+        backtrack(ans, cur + ")", open, close + 1, max);
+    }
 }
