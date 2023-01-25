9481c0e35dbe1c374d03c81dcf7c614b19dfa545
Qiao Yang Han <qyhwork@gmail.com>
2023-01-25 12:17:11 -0500

    Worked on valid parentheses

---
+++ validParentheses.js
@@ -0,0 +1,23 @@
+var isValid = function (s) {
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (!check("(", ")", 0, char) ||
+            !check("{", "}", 1, char) ||
+            !check("[", "]", 2, char)) {
+            return false;
+        }
+    }
+    return count.every(ele => ele === 0);
+};
+
+const count = [0, 0, 0];
+
+const check = (open, closed, idx, char) => {
+    if (char === open || char === closed) {
+        char === open ? count[idx] += 1 : count[idx] -= 1;
+        if (count[idx] < 0) {
+            return false;
+        }
+    }
+    return true;
+}
