fd658808aa882f14261bf8d28a36f7d79422d843
Qiao Yang Han <qyhwork@gmail.com>
2023-02-03 02:26:54 -0500

    Worked on Reverse Nodes in Even Length Groups

--- reverseNodesInEvenLengthGroups.js
+++ reverseNodesInEvenLengthGroups.js
@@ -1,3 +1,35 @@
 var reverseEvenLengthGroups = function (head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    let l = 2;
+    let n = 0;
+    let node = head;
+    let prev = head;
+    while (node.next) {
+        node = node.next;
+        n += 1;
+        if (n === l) {
+            if (l % 2 === 0) {
+                let end = node.next;
+                let reverse = node.next;
+                let curr = prev.next;
+                while (curr != end) {
+                    let currNext = curr.next;
+                    curr.next = reverse;
+                    reverse = curr;
+                    curr = currNext;
+                }
+                let tmp = prev.next;
+                prev.next = reverse;
+                prev = tmp;
+                l += 1;
+                node = prev;
+                n = 0;
+            }
+            else {
 
+            }
+        }
+    }
 };
