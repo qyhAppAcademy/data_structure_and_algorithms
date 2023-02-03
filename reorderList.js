5b613525e3d8394d632e11d787cdca0b0679ec3e
Qiao Yang Han <qyhwork@gmail.com>
2023-02-02 20:14:10 -0500

    Worked on Reorder List

--- reorderList.js
+++ reorderList.js
@@ -10,8 +10,26 @@ var reorderList = function (head) {
     }
     let first = head;
     let second = slow;
+    let reversedSecond = reverseNodes(second);
+    while (reversedSecond && reversedSecond.next) {
+        let firstNext = first.next;
+        let reversedSecondNext = reversedSecond.next;
+        first.next = reversedSecond;
+        reversedSecond.next = firstNext;
+        first = firstNext;
+        reversedSecond = reversedSecondNext;
+    }
+    return head;
 };
 
 const reverseNodes = (head) => {
-
+    let previous = null;
+    let current = head;
+    while (current) {
+        let next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    return previous;
 }
