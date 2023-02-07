4e2bbaa883d250a78c9c02fc4e9749aa6cc66249
Qiao Yang Han <qyhwork@gmail.com>
2023-02-07 16:43:37 -0500

    Worked on Merge Sorted Array

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,3 +1,27 @@
 var merge = function (nums1, m, nums2, n) {
-
+    let p = m + n - 1;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1 -= 1;
+        }
+        else {
+            nums1[p] = nums2[p2];
+            p2 -= 1;
+        }
+        p -= 1;
+    }
+    while (p1 >= 0) {
+        nums1[p] = nums1[p1];
+        p1 -= 1;
+        p -= 1;
+    }
+    while (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2 -= 1;
+        p -= 1;
+    }
+    return nums1;
 };
