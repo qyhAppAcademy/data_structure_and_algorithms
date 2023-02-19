0636c300b706f07a740b6944481fa6b430ecc947
Qiao Yang Han <qyhwork@gmail.com>
2023-02-19 14:16:16 -0500

    Worked on PILYTIX Opportunity

--- PILYTIX_Opportunity.js
+++ PILYTIX_Opportunity.js
@@ -301,3 +301,14 @@
 // Respectfully,
 
 // Qiao
+
+// Timeline
+// 02/10/2023 2 hrs Reading assignment and planning I
+// 02/11/2023 2 hrs Reading assignment and planning II
+// 02/12/2023 8 hrs Displaying probability history using Sunburst Chart
+// 02/13/2023 6 hrs Displaying Pilytix Prob and Rep Prob using Bar/Line Chart
+// 02/14/2023 6 hrs Displaying other data using Material UI components
+// 02/15/2023 6 hrs Implementing back/next feature
+// 02/16/2023 6 hrs Improving display and refining code
+// 02/17/2023 6 hrs Making Material UI components responsive
+// 02/18/2023 6 hrs Making Bar/Line Chart and Sunburst Chart responsive
