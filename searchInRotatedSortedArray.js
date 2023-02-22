62a17ff671d472d50e6c347ab74acea3ddb3da2d
Qiao Yang Han <qyhwork@gmail.com>
2023-02-22 15:38:46 -0500

    Worked on Search In Rotated Sorted Array

--- searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray.js
@@ -1,3 +1,28 @@
 var search = function (nums, target) {
-
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2.0);
+        console.log(`low: ${low} mid: ${mid} high: ${high}`)
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[low] < nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid + 1] <= target && target <= nums[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
 };
