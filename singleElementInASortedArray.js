cd5e3accabf7de9a74a0dcc9d32697bd6d374d18
Qiao Yang Han <qyhwork@gmail.com>
2023-02-23 18:47:07 -0500

    Worked on Single Element in a Sorted Array

--- singleElementInASortedArray.js
+++ singleElementInASortedArray.js
@@ -1,2 +1,24 @@
 var singleNonDuplicate = function (nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2.0);
+        if (mid % 2 === 0) {
+            if (nums[mid] === nums[mid + 1]) {
+                low = mid + 2;
+            }
+            else {
+                high = mid;
+            }
+        }
+        else {
+            if (nums[mid] === nums[mid - 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return low;
 };
