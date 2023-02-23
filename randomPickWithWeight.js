b3bbeb6c846036129899379f01e7e5e4af9a4be1
Qiao Yang Han <qyhwork@gmail.com>
2023-02-22 23:02:32 -0500

    Worked on Random Pick With Weight

--- randomPickWithWeight.js
+++ randomPickWithWeight.js
@@ -11,5 +11,17 @@ var Solution = function (w) {
  * @return {number}
  */
 Solution.prototype.pickIndex = function () {
-
+    let randomNum = Math.random() * this.line[this.line.length - 1];
+    let start = 0;
+    let end = this.length - 1;
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2.0);
+        if (randomNum <= this.line[mid]) {
+            return mid;
+        }
+        else {
+            start = mid;
+        }
+    }
+    return this.line.length - 1;
 };
