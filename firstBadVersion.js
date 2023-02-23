de26cabef64fc97f56eb5b875854e58251e960f8
Qiao Yang Han <qyhwork@gmail.com>
2023-02-22 19:47:28 -0500

    Worked on First Bad Version

--- firstBadVersion.js
+++ firstBadVersion.js
@@ -4,6 +4,17 @@ var solution = function (isBadVersion) {
      * @return {integer} The first bad version
      */
     return function (n) {
-
+        let start = 1;
+        let end = n;
+        while (start < end) {
+            let mid = Math.floor((start + end) / 2.0);
+            if (isBadVersion(mid)) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
     };
 };
