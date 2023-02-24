0de3a5d5781e1e54904761cda40854940ea50602
Qiao Yang Han <qyhwork@gmail.com>
2023-02-23 21:11:35 -0500

    Worked on Search in Rotated Sorted Array II

--- searchInRotatedSortedArrayII.js
+++ searchInRotatedSortedArrayII.js
@@ -10,21 +10,23 @@ var search = function (nums, target) {
         if (nums[start] === nums[end]) {
             start += 1;
             end -= 1;
+            continue;
         }
-        else if (nums[start] > nums[end]) {
-            if (nums[mid] < target && target <= nums[end]) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
-        }
-        else if (nums[start] < nums[end]) {
+
+        if (nums[start] < nums[mid]) {
             if (nums[start] <= target && target < nums[mid]) {
                 end = mid - 1;
             }
             else {
                 start = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
             }
         }
     }
