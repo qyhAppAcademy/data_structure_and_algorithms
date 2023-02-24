d9551ef2dd8705c1998c5296dca8b65d3a4c837c
Qiao Yang Han <qyhwork@gmail.com>
2023-02-24 14:27:42 -0500

    Worked on Subsets

--- subsets.js
+++ subsets.js
@@ -1,5 +1,17 @@
 var subsets = function (nums) {
     const size = 2 ** nums.length;
+    const subsets = [];
+    for (let i = 0; i < size; i++) {
+        const subset = new Set();
+        for (let j = 0; j < nums.length; j++) {
+            const bit = getBit(i, j);
+            if (bit === 1) {
+                subset.add(nums[j]);
+            }
+        }
+        subsets.push(Array.from(subset));
+    }
+    return subsets;
 };
 
 const getBit = (num, bit) => {
