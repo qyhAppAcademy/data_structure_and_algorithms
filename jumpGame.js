73f28c3b48f6e4acef4fc99306bc5b93363b3efe
Qiao Yang Han <qyhwork@gmail.com>
2023-02-25 11:27:20 -0500

    Worked on Jump Game

--- jumpGame.js
+++ jumpGame.js
@@ -2,14 +2,14 @@ var canJump = function (nums) {
     if (nums.length <= 1) {
         return true;
     }
-    return canJumpRec(nums, nums.length - 2);
+    return canJumpRec(nums, nums.length - 1);
 };
 
 const canJumpRec = (nums, currentIndex) => {
     if (currentIndex === 0) {
         return true;
     }
-    for (let step = 1; step <= nums[currentIndex]; step++) {
+    for (let step = nums[currentIndex - 1]; step > 0; step--) {
         if (canJumpRec(nums, currentIndex - step)) {
             return true;
         }
