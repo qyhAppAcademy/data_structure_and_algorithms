c8e8a06218173d6054ec8803a98e7c74e18cf8cc
Qiao Yang Han <qyhwork@gmail.com>
2023-02-24 21:57:41 -0500

    Worked on Permutations

--- permutations.js
+++ permutations.js
@@ -1,3 +1,27 @@
 var permute = function (nums) {
-
+    const result = [];
+    permutation(nums, 0, result);
+    return result;
 };
+
+const permutation = (nums, currentIndex, result) => {
+    if (currentIndex === nums.length - 1) {
+        const arr = [];
+        for (let num of nums) {
+            arr.push(num);
+        }
+        result.push(arr);
+        return;
+    }
+    for (let i = currentIndex; i < nums.length; i++) {
+        swapIndex(nums, currentIndex, i);
+        permutation(nums, currentIndex + 1, result);
+        swapIndex(nums, i, currentIndex);
+    }
+}
+
+const swapIndex = (nums, i, j) => {
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
