d9fe830fa0324f7eb35bea16b49b7e5f48721d9a
Qiao Yang Han <qyhwork@gmail.com>
2023-02-27 12:45:32 -0500

    Worked on Jump Game II

--- jumpGameII.js
+++ jumpGameII.js
@@ -1,3 +1,23 @@
 var jump = function (nums) {
-
+    if (nums.length === 0) {
+        return 0;
+    }
+    let reached = false;
+    let idx = 0;
+    let steps = nums[0];
+    let count = 0;
+    while (!reached) {
+        let range = 0;
+        for (let i = idx + 1; i <= idx + steps; i++) {
+            if (i === nums.length) {
+                reached = true;
+                break;
+            }
+            range = Math.max(range, i + nums[i]);
+        }
+        idx += steps;
+        steps = range - idx;
+        count += 1;
+    }
+    return count;
 };
