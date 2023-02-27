996d99edfcfcf9b31ba614231720084e14a8e936
Qiao Yang Han <qyhwork@gmail.com>
2023-02-27 16:52:34 -0500

    Worked on N-th Tribonacci Number

--- nthTribonacciNumber.js
+++ nthTribonacciNumber.js
@@ -1,5 +1,13 @@
 var tribonacci = function (n) {
-    let t0 = 0;
-    let t1 = 1;
-    let t2 = 1;
+    if (n === 0) {
+        return 0;
+    }
+    if (n <= 2) {
+        return 1;
+    }
+    let t = [0, 1, 1];
+    for (let i = 3; i <= n; i++) {
+        t[i] = t[i - 1] + t[i - 2] + t[i - 3];
+    }
+    return t[n];
 };
