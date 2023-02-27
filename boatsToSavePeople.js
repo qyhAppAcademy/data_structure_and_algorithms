879b1e824034ec2f810ad101e47f0bf3ac7dc815
Qiao Yang Han <qyhwork@gmail.com>
2023-02-27 08:42:42 -0500

    Worked on Boats to Save People

--- boatsToSavePeople.js
+++ boatsToSavePeople.js
@@ -1,2 +1,22 @@
 var numRescueBoats = function (people, limit) {
+    people.sort();
+    let light = 0;
+    let heavy = people.length - 1;
+    let count = 0;
+    console.log(people);
+    while (light < heavy) {
+        if (people[light] + people[heavy] <= limit) {
+            count += 1;
+            light += 1;
+            heavy -= 1;
+        }
+        else {
+            count += 1;
+            heavy -= 1;
+        }
+    }
+    if (light === heavy) {
+        count += 1;
+    }
+    return count;
 };
