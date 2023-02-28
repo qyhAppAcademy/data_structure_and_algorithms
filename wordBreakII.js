61c60a13156b2c8bcc0a1869df55c5094561efc6
Qiao Yang Han <qyhwork@gmail.com>
2023-02-28 18:31:21 -0500

    Worked on Word Break II

--- wordBreakII.js
+++ wordBreakII.js
@@ -1,4 +1,37 @@
 var wordBreak = function (s, wordDict) {
-    const dict = new Set(["cook", "cook"]);
-    console.log(dict.entries());
+    const result = {};
+    helper(s, new Set(wordDict), result);
+    return result[s];
 };
+
+const helper = (s, dict, result) => {
+    if (s.length === 0) {
+        return [];
+    }
+
+    if (Object.keys(result).includes(s)) {
+        return result[s];
+    }
+
+    const res = [];
+    for (let word of dict.entries()) {
+        word = word[0];
+
+        if (!s.startsWith(word)) {
+            continue;
+        }
+
+        if (word.length === s.length) {
+            res.push(word);
+            break;
+        }
+        else {
+            const resultsOfSubstr = helper(s.substring(word.length), dict, result);
+            for (let resultOfSubstr of resultsOfSubstr) {
+                res.push(word + " " + resultOfSubstr);
+            }
+        }
+    }
+    result[s] = res;
+    return res;
+}
