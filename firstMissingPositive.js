d8c44288739dabf83ad29b88f842730152236489
Qiao Yang Han <qyhwork@gmail.com>
2023-03-02 18:13:18 -0500

    Worked on First Missing Positive

--- firstMissingPositive.js
+++ firstMissingPositive.js
@@ -1,19 +1,17 @@
 var firstMissingPositive = function (nums) {
     let idx = 0;
     while (idx < nums.length) {
-        if (nums[idx] != idx && idx < nums.length) {
-            let tmp = nums[nums[idx]];
-            nums[nums[idx]] = nums[idx];
+        while (nums[idx] - 1 !== idx && 0 <= nums[idx] - 1 && nums[idx] - 1 <= nums.length) {
+            let tmp = nums[nums[idx] - 1];
+            nums[nums[idx] - 1] = nums[idx];
             nums[idx] = tmp;
         }
-        else {
-            idx += 1;
-        }
+        idx += 1;
     }
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== i) {
-            return i;
+        if (nums[i] - 1 !== i) {
+            return i + 1;
         }
     }
-    return 1
+    return nums.length + 1;
 };
