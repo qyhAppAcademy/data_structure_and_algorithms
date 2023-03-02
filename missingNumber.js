0bc88ee79b821c97604d39b0139934647e5e6a40
Qiao Yang Han <qyhwork@gmail.com>
2023-03-02 01:45:38 -0500

    Worked on Missing Number

--- missingNumber.js
+++ missingNumber.js
@@ -1,3 +1,22 @@
 var missingNumber = function (nums) {
-
+    let idx = 0;
+    while (idx < nums.length) {
+        if (nums[idx] != idx) {
+            let tmp = nums[nums[idx]];
+            nums[nums[idx]] = nums[idx];
+            nums[idx] = tmp;
+            if (nums[idx] > nums.length - 1) {
+                idx += 1;
+            }
+        }
+        else {
+            idx += 1;
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i) {
+            return i;
+        }
+    }
+    return nums.length
 };
