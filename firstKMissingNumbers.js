23b2e5564655ed55aba9e50e8f16c10ed2e4d95b
Qiao Yang Han <qyhwork@gmail.com>
2023-03-02 21:08:39 -0500

    Worked on First K Missing Numbers

--- firstKMissingNumbers.js
+++ firstKMissingNumbers.js
@@ -1,7 +1,7 @@
 export function firstKMissingNumbers(arr, k) {
     // your code will replace this placeholder return statement
     for (let i = 0; i < arr.length; i++) {
-        while (arr[i] - 1 !== i && arr[arr[i] - 1] !== arr[i]) {
+        while (arr[i] > 0 && arr[i] - 1 !== i && arr[arr[i] - 1] !== arr[i]) {
             let tmp = arr[arr[i] - 1];
             arr[arr[i] - 1] = arr[i];
             arr[i] = tmp;
@@ -9,14 +9,13 @@ export function firstKMissingNumbers(arr, k) {
     }
     const result = [];
     let count = 0;
+    console.log(arr);
     for (let i = 1; i <= arr.length; i++) {
-        if (i === arr.length || arr[i] > 0) {
-            for (let j = (arr[i - 1] < 0 ? 1 : arr[i - 1] + 1); j < (i === arr.length ? arr[i - 1] + k + 1 : arr[i]); j++) {
-                result.push(j);
-                count += 1;
-                if (count === k) {
-                    return result;
-                }
+        for (let j = (arr[i - 1] < 0 ? 1 : arr[i - 1] + 1); j < (i === arr.length ? arr[i - 1] + k + 1 : arr[i]); j++) {
+            result.push(j);
+            count += 1;
+            if (count === k) {
+                return result;
             }
         }
     }
