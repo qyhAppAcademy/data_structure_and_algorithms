72d0ec09af6fad0edfa1dc955e1c6cc6c2f8df8a
Qiao Yang Han <qyhwork@gmail.com>
2023-03-03 15:42:09 -0500

    Worked on Find Duplicate Floyd

--- findDuplicate.js
+++ findDuplicate.js
@@ -13,3 +13,19 @@ var findDuplicate = function (nums) {
     }
     return nums.length + 1;
 };
+
+var findDuplicateFloyd = function (nums) {
+    let slow = nums[0];
+    let fast = nums[nums[0]];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    slow = nums[0];
+    fast = nums[fast];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+};
