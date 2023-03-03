083aa115184a213a6da3eee6b511267c950fa2d0
Qiao Yang Han <qyhwork@gmail.com>
2023-03-02 19:19:27 -0500

    Worked on First Missing Positive

--- findDuplicate.js
+++ findDuplicate.js
@@ -1,2 +1,11 @@
 var findDuplicate = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] - 1 !== i && nums[i] !== nums[nums[i] - 1]) {
+            let tmp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = tmp;
+        }
+    }
+
+    return nums.length + 1;
 };
