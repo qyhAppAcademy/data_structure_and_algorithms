4c0339d28865fd56d06ba9fc27507ddf23e3a329
Qiao Yang Han <qyhwork@gmail.com>
2023-03-04 17:04:29 -0500

    Worked on Find Compliation Order

--- findCompilationOrder.js
+++ findCompilationOrder.js
@@ -1,3 +1,24 @@
 const findCompilationOrder = (dependencies) => {
-    
+    let graph = {};
+    let inDegrees = {};
+    dependencies.forEach(dependency => {
+        const parent = dependency[1];
+        const child = dependency[0];
+        if (graph[parent] === undefined) {
+            graph[parent] = [];
+        }
+        graph[parent].push(child);
+        if (inDegrees[parent] === undefined) {
+            inDegrees[parent] = 0;
+        }
+        if (inDegrees[child] === undefined) {
+            inDegrees[child] = 0;
+        }
+        inDegrees[child] += 1;
+    });
+
+    if (Object.keys(graph).length === 0) {
+        return [];
+    }
+
 }
