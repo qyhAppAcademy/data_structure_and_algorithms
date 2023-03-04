9578ef2345e9a24601fc372ab2c53e2c2ba987c6
Qiao Yang Han <qyhwork@gmail.com>
2023-03-04 17:07:25 -0500

    Worked on Find Compliation Order

--- findCompilationOrder.js
+++ findCompilationOrder.js
@@ -1,3 +1,30 @@
+class Queue {
+    constructor() {
+        this.elements = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+    enqueue(element) {
+        this.elements[this.tail] = element;
+        this.tail++;
+    }
+    dequeue() {
+        const item = this.elements[this.head];
+        delete this.elements[this.head];
+        this.head++;
+        return item;
+    }
+    peek() {
+        return this.elements[this.head];
+    }
+    get length() {
+        return this.tail - this.head;
+    }
+    get isEmpty() {
+        return this.length === 0;
+    }
+}
+
 const findCompilationOrder = (dependencies) => {
     let graph = {};
     let inDegrees = {};
@@ -20,5 +47,4 @@ const findCompilationOrder = (dependencies) => {
     if (Object.keys(graph).length === 0) {
         return [];
     }
-
 }
