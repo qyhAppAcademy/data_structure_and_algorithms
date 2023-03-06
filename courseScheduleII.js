efd2cc3efeee5bb36400c8f04464cf780b9ff56b
Qiao Yang Han <qyhwork@gmail.com>
2023-03-06 00:02:06 -0500

    Worked on Course Schedule II

--- courseScheduleII.js
+++ courseScheduleII.js
@@ -26,5 +26,34 @@ class Queue2 {
 }
 
 var findOrder = function (numCourses, prerequisites) {
-
+    const graph = {};
+    const inDegrees = {};
+    for (let i = 0; i < numCourses; i++) {
+        graph[i] = [];
+        inDegrees[i] = 0;
+    }
+    for (let pre of prerequisites) {
+        let parent = pre[1];
+        let child = pre[0];
+        graph[parent].push(child);
+        inDegrees[child] += 1;
+    }
+    const coursesOrder = [];
+    const sources = new Queue2();
+    for (let vertex of Object.keys(inDegrees)) {
+        if (inDegrees[vertex] === 0) {
+            sources.enqueue(vertex);
+        }
+    }
+    while (sources.length !== 0) {
+        const source = sources.dequeue();
+        coursesOrder.push(source);
+        for (let child of graph[source]) {
+            inDegrees[child] -= 1;
+            if (inDegrees[child] === 0) {
+                sources.enqueue(child);
+            }
+        }
+    }
+    return coursesOrder.length === numCourses ? coursesOrder : [];
 };
