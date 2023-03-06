c934ee7973227c679b9bd71ed3dd4e41878d2d3d
Qiao Yang Han <qyhwork@gmail.com>
2023-03-06 00:46:49 -0500

    Worked on Find All Possible Recipes from Given Supplies

--- courseScheduleII.js
+++ courseScheduleII.js
@@ -25,35 +25,43 @@ class Queue2 {
     }
 }
 
-var findOrder = function (numCourses, prerequisites) {
+var findAllRecipes = function (recipes, ingredients, supplies) {
     const graph = {};
     const inDegrees = {};
-    for (let i = 0; i < numCourses; i++) {
-        graph[i] = [];
-        inDegrees[i] = 0;
-    }
-    for (let pre of prerequisites) {
-        let parent = pre[1];
-        let child = pre[0];
-        graph[parent].push(child);
-        inDegrees[child] += 1;
-    }
-    const coursesOrder = [];
-    const sources = new Queue2();
-    for (let vertex of Object.keys(inDegrees)) {
-        if (inDegrees[vertex] === 0) {
-            sources.enqueue(vertex);
+    for (let i = 0; i < ingredients.length; i++) {
+        let child = recipes[i];
+        let ingredient = ingredients[i];
+        for (let j = 0; j < ingredient.length; j++) {
+            let parent = ingredient[j];
+            if (graph[parent] === undefined) {
+                graph[parent] = [];
+            }
+            graph[parent].push(child);
+
+            if (inDegrees[child] === undefined) {
+                inDegrees[child] = 0;
+            }
+            inDegrees[child] += 1;
         }
+    }
+    console.log(graph);
+    console.log(inDegrees);
+    const result = [];
+    const sources = new Queue2();
+    for (let vertex of supplies) {
+        sources.enqueue(vertex);
     }
     while (sources.length !== 0) {
         const source = sources.dequeue();
-        coursesOrder.push(source);
-        for (let child of graph[source]) {
-            inDegrees[child] -= 1;
-            if (inDegrees[child] === 0) {
-                sources.enqueue(child);
+        if (graph[source] !== undefined) {
+            for (let child of graph[source]) {
+                inDegrees[child] -= 1;
+                if (inDegrees[child] === 0) {
+                    sources.enqueue(child);
+                    result.push(child);
+                }
             }
         }
     }
-    return coursesOrder.length === numCourses ? coursesOrder : [];
+    return result;
 };
