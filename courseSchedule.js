ea155d186c3fa5158dd989cd2856b7df0b0183dc
Qiao Yang Han <qyhwork@gmail.com>
2023-03-05 23:49:24 -0500

    Worked on Verifying an Alien Dictionary

--- courseSchedule.js
+++ courseSchedule.js
@@ -1,4 +1,4 @@
-class Queue {
+class Queue2 {
     constructor() {
         this.elements = {};
         this.head = 0;
@@ -26,5 +26,43 @@ class Queue {
 }
 
 var canFinish = function (numCourses, prerequisites) {
+    const graph = {};
+    const inDegrees = {};
+    for (let pre of prerequisites) {
+        let parent = pre[1];
+        let child = pre[0];
 
+        if (graph[parent] === undefined) {
+            graph[parent] = [];
+        }
+        graph[parent].push(child);
+
+        if (inDegrees[parent] === undefined) {
+            inDegrees[parent] = 0;
+        }
+        if (inDegrees[child] === undefined) {
+            inDegrees[child] = 0;
+        }
+        inDegrees[child] += 1;
+    }
+    let count = 0;
+    const sources = new Queue2();
+    for (let vertex of Object.keys(inDegrees)) {
+        if (inDegrees[vertex] === 0) {
+            sources.enqueue(vertex);
+        }
+    }
+    while (sources.length !== 0) {
+        const source = sources.dequeue();
+        count += 1;
+        if (graph[source] !== undefined) {
+            for (let child of graph[source]) {
+                inDegrees[child] -= 1;
+                if (inDegrees[child] === 0) {
+                    sources.enqueue(child);
+                }
+            }
+        }
+    }
+    return count === numCourses;
 };
