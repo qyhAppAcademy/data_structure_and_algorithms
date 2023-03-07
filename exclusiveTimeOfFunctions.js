e9bb6cdf287823f84c6461346db82ed90dc8247b
Qiao Yang Han <qyhwork@gmail.com>
2023-03-07 17:50:29 -0500

    Worked on Exclusive Time of Functions

---
+++ exclusiveTimeOfFunctions.js
@@ -0,0 +1,29 @@
+var exclusiveTime = function (n, logs) {
+    const result = new Array(n).fill(0);
+    const stack = [logs[0].split(":")];
+    for (let i = 1; i < logs.length; i++) {
+        let [id, status, time] = logs[i].split(":");
+        let top = stack[stack.length - 1];
+        if (id === top[0]) {
+            if (status === "end") {
+                result[id] += time - top[2] + 1;
+                stack.pop();
+                if (stack[stack.length - 1] !== undefined) {
+                    stack[stack.length - 1][2] = time;
+                }
+            }
+            else {
+                result[id] += time - top[2] - 1;
+                stack[stack.length - 1][2] = time;
+                stack.push([id, status, time]);
+            }
+
+        }
+        else {
+            result[top[0]] += time - top[2] - 1;
+            stack[stack.length - 1][2] = time;
+            stack.push([id, status, time]);
+        }
+    }
+    return result;
+};
