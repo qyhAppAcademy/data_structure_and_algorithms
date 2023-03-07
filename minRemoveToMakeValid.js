466981c6d60893d420d93ee675844aa7f056c1dd
Qiao Yang Han <qyhwork@gmail.com>
2023-03-07 01:53:13 -0500

    Worked on Min Remove To Make Valid

--- minRemoveToMakeValid.js
+++ minRemoveToMakeValid.js
@@ -1,3 +1,23 @@
 var minRemoveToMakeValid = function (s) {
-
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if ("()".includes(s[i])) {
+            if (s[stack[stack.length - 1]] === "(" && s[i] === ")") {
+                stack.pop();
+            }
+            else {
+                stack.push(i);
+            }
+        }
+    }
+    let result = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[stack[0]] === s[i]) {
+            stack.shift();
+        }
+        else {
+            result += s[i];
+        }
+    }
+    return result;
 };
