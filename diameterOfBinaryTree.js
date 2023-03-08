00a3f09f9edd54d08b5f7492f6dd7bcdabf47682
Qiao Yang Han <qyhwork@gmail.com>
2023-03-08 16:37:03 -0500

    Worked on Diameter of Binary Tree

--- diameterOfBinaryTree.js
+++ diameterOfBinaryTree.js
@@ -1,3 +1,35 @@
 var diameterOfBinaryTree = function (root) {
-    s
+    if (!root) {
+        return 0;
+    }
+    let stack;
+    let maxLeft = 0;
+    if (root.left) {
+        stack = [[root.left, 1]];
+        while (stack.length > 0) {
+            const [top, depth] = stack.pop();
+            maxLeft = Math.max(maxLeft, depth);
+            if (top.right) {
+                stack.push([top.right, depth + 1]);
+            }
+            if (top.left) {
+                stack.push([top.left, depth + 1]);
+            }
+        }
+    }
+    let maxRight = 0;
+    if (root.right) {
+        stack = [[root.right, 1]];
+        while (stack.length > 0) {
+            const [top, depth] = stack.pop();
+            maxRight = Math.max(maxRight, depth);
+            if (top.right) {
+                stack.push([top.right, depth + 1]);
+            }
+            if (top.left) {
+                stack.push([top.left, depth + 1]);
+            }
+        }
+    }
+    return maxLeft + maxRight;
 };
