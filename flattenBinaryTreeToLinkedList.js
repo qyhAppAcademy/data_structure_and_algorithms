913f412478634a8874a6dcf7f380ef31e29fc0d2
Qiao Yang Han <qyhwork@gmail.com>
2023-03-08 15:55:36 -0500

    Worked on Flatten Binary Tree to Linked List

--- flattenBinaryTreeToLinkedList.js
+++ flattenBinaryTreeToLinkedList.js
@@ -1,3 +1,20 @@
 var flatten = function (root) {
-
+    if (!root) {
+        return [];
+    }
+    const result = [];
+    const stack = [root];
+    let itr = new TreeNode(0, null, root);
+    while (stack.length > 0) {
+        let top = stack.pop();
+        if (top.right) {
+            stack.push(top.right);
+        }
+        if (top.left) {
+            stack.push(top.left);
+        }
+        itr.left = null;
+        itr.right = top;
+        itr = itr.right;
+    }
 };
