d66ed68a2ba3319cb5801f144609a5a0205446a9
Qiao Yang Han <qyhwork@gmail.com>
2023-03-09 02:23:49 -0500

    Worked on Invert Binary Tree

--- invertedBinaryTree.js
+++ invertedBinaryTree.js
@@ -1,3 +1,9 @@
 var invertTree = function (root) {
-
+    if (!root) {
+        return null;
+    }
+    let tmp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(tmp);
+    return root;
 };
