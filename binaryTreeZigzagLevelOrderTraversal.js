335e4a02a40b090a090bcf78c7dfe7e549c791e2
Qiao Yang Han <qyhwork@gmail.com>
2023-03-09 16:47:36 -0500

    Worked on Binary Tree Level Order Traversal

--- binaryTreeZigzagLevelOrderTraversal.js
+++ binaryTreeZigzagLevelOrderTraversal.js
@@ -1,3 +1,28 @@
 var zigzagLevelOrder = function (root) {
-
+    if (!root) {
+        return [];
+    }
+    const result = [];
+    let level = -1;
+    const queue = [[root, 0]];
+    while (queue.length > 0) {
+        let [node, lvl] = queue.shift();
+        if (lvl > level) {
+            result.push([]);
+            level += 1;
+        }
+        if (level % 2 === 0) {
+            result[level].push(node.val);
+        }
+        else {
+            result[level].unshift(node.val);
+        }
+        if (node.left) {
+            queue.push([node.left, lvl + 1]);
+        }
+        if (node.right) {
+            queue.push([node.right, lvl + 1]);
+        }
+    }
+    return result;
 };
