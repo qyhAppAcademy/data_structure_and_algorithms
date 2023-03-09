1075ce5c98d65cbbb5a65894819cfe9301718993
Qiao Yang Han <qyhwork@gmail.com>
2023-03-09 03:27:05 -0500

    Worked on Maximum Depth of Binary

--- maximumDepthOfBinary.js
+++ maximumDepthOfBinary.js
@@ -1,3 +1,8 @@
 var maxDepth = function (root) {
-
+    if (!root) {
+        return 0;
+    }
+    let lm = maxDepth(root.left);
+    let rm = maxDepth(root.right);
+    return Math.max(lm, rm) + 1;
 };
