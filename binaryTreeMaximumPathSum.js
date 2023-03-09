5b63d27579592f086e5d7a0f4d1e00e63caa10ac
Qiao Yang Han <qyhwork@gmail.com>
2023-03-09 03:23:31 -0500

    Worked on Binary Tree Maximum Path Sum

--- binaryTreeMaximumPathSum.js
+++ binaryTreeMaximumPathSum.js
@@ -4,5 +4,12 @@ var maxPathSum = function (root) {
 };
 
 const helper = (root) => {
-
+    if (!root) {
+        return [0, Number.NEGATIVE_INFINITY];
+    }
+    let [lp, lm] = helper(root.left);
+    let [rp, rm] = helper(root.right);
+    let p = Math.max(lp + root.val, rp + root.val, root.val);
+    let m = Math.max(lm, rm, lp + rp + root.val, p, root.val);
+    return [p, m];
 }
