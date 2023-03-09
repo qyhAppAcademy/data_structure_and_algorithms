db93d9985fdf4bb8651620fb9e6e32f8d3cd3a71
Qiao Yang Han <qyhwork@gmail.com>
2023-03-08 23:09:08 -0500

    Worked on Diameter of Binary Tree

--- serializeAndDeserializeBinaryTree.js
+++ serializeAndDeserializeBinaryTree.js
@@ -18,15 +18,15 @@ var serialize = function (root) {
     return result;
 };
 
-const serializeHelper = (root, result) => {
-    if (!root) {
-        result.push(null);
-        return;
-    }
-    result.push(root.val);
-    serializeHelper(root.left, result);
-    serializeHelper(root.right, result);
-}
+// const serializeHelper = (root, result) => {
+//     if (!root) {
+//         result.push(null);
+//         return;
+//     }
+//     result.push(root.val);
+//     serializeHelper(root.left, result);
+//     serializeHelper(root.right, result);
+// }
 
 /**
  * Decodes your encoded data to tree.
@@ -35,25 +35,25 @@ const serializeHelper = (root, result) => {
  * @return {TreeNode}
  */
 var deserialize = function (data) {
-    if (data.length === 0) {
-        return null;
-    }
-    return deserializeHelper(data);
+    // if (data.length === 0) {
+    //     return null;
+    // }
+    // return deserializeHelper(data);
 };
 
-const deserializeHelper = (data) => {
-    if (data.length === 0) {
-        return null;
-    }
-    let datum = data.shift();
-    if (datum === null) {
-        return null;
-    }
-    const node = new TreeNode(datum);
-    node.left = deserializeHelper(data);
-    node.right = deserializeHelper(data);
-    return node;
-}
+// const deserializeHelper = (data) => {
+//     if (data.length === 0) {
+//         return null;
+//     }
+//     let datum = data.shift();
+//     if (datum === null) {
+//         return null;
+//     }
+//     const node = new TreeNode(datum);
+//     node.left = deserializeHelper(data);
+//     node.right = deserializeHelper(data);
+//     return node;
+// }
 
 /**
  * Your functions will be called as such:
