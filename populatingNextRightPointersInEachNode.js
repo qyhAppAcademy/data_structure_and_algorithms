a2730bd3f308063837b02fe533810924df2341b0
Qiao Yang Han <qyhwork@gmail.com>
2023-03-09 17:24:51 -0500

    Worked on Populating Next Right Pointers In Each Node

--- populatingNextRightPointersInEachNode.js
+++ populatingNextRightPointersInEachNode.js
@@ -1,3 +1,26 @@
 var connect = function (root) {
-
+    if (!root) {
+        return root;
+    }
+    let prev = null;
+    let level = -1;
+    const queue = [[root, 0]];
+    while (queue.length > 0) {
+        let [node, lvl] = queue.shift();
+        if (lvl > level) {
+            level += 1;
+            prev = node;
+        }
+        else {
+            prev.next = node;
+            prev = prev.next;
+        }
+        if (node.left) {
+            queue.push([node.left, lvl + 1]);
+        }
+        if (node.right) {
+            queue.push([node.right, lvl + 1]);
+        }
+    }
+    return root;
 };
