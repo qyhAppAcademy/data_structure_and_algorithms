ec9d3e497f867565dc55026e4b2a79ef189cde9e
Qiao Yang Han <qyhwork@gmail.com>
2023-03-11 01:00:24 -0500

    Worked on Replace Words

---
+++ replaceWords.js
@@ -0,0 +1,44 @@
+var Node = function () {
+    this.children = {};
+    this.word = undefined;
+};
+
+var Trie = function () {
+    this.root = new Node();
+};
+
+Trie.prototype.insert = function (word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+        let c = word[i];
+        if (node.children[c] === undefined) {
+            node.children[c] = new Node();
+        }
+        node = node.children[c];
+    }
+    node.word = word;
+};
+
+Trie.prototype.search = function (word) {
+    let node = this.root;
+    for (let i = 0; i < word.length; i++) {
+        let c = word[i];
+        if (node.children[c] === undefined) {
+            if (node.word === undefined) {
+                return word;
+            }
+            else {
+                return node.word;
+            }
+        }
+        else {
+            if (node.word === undefined) {
+                node = node.children[c];
+            }
+            else {
+                return node.word;
+            }
+        }
+    }
+    return node.word === undefined ? word : node.word;
+};
