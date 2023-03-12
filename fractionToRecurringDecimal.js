747b1c0464d90fe0d92c6b788c239742daedcb24
Qiao Yang Han <qyhwork@gmail.com>
2023-03-12 18:03:22 -0400

    Worked on Fraction To Recurring Decimal

--- fractionToRecurringDecimal.js
+++ fractionToRecurringDecimal.js
@@ -1,6 +1,53 @@
+/**
+ * @param {number} numerator
+ * @param {number} denominator
+ * @return {string}
+ */
 var fractionToDecimal = function (numerator, denominator) {
     const hashMap = {};
-    let result = "";
+    let result = numerator / denominator >= 0 ? "" : "-";
+    numerator = Math.abs(numerator);
+    denominator = Math.abs(denominator);
+    let q = Math.floor(numerator / denominator);
+    let r = numerator % denominator;
+    result += q.toString();
+    if (r > 0) {
+        result += ".";
+        r *= 10;
+        hashMap[r] = result.length;
+    }
+    while (r % denominator > 0) {
+        q = Math.floor(r / denominator);
+        r = r % denominator;
+        result += q.toString();
+        r *= 10;
+        if (hashMap[r] === undefined) {
+            hashMap[r] = result.length;
+        }
+        else {
+            break;
+        }
+    }
+    if (r === 0) {
+        return result;
+    }
+    else if (r % denominator === 0) {
+        result += Math.floor(r / denominator).toString();
+        return result;
+    }
+    else {
+        return result.substring(0, hashMap[r]) + "(" + result.substring(hashMap[r]) + ")";
+    }
+};/**
+ * @param {number} numerator
+ * @param {number} denominator
+ * @return {string}
+ */
+var fractionToDecimal = function (numerator, denominator) {
+    const hashMap = {};
+    let result = numerator / denominator >= 0 ? "" : "-";
+    numerator = Math.abs(numerator);
+    denominator = Math.abs(denominator);
     let q = Math.floor(numerator / denominator);
     let r = numerator % denominator;
     result += q.toString();
