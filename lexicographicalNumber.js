8c8a1afd15225131953f608faa6355d5a594a539
Qiao Yang Han <qyhwork@gmail.com>
2023-03-12 03:01:14 -0400

    Worked on Lexicographical Numbers

--- lexicographicalNumber.js
+++ lexicographicalNumber.js
@@ -9,3 +9,15 @@ var TrieNode = function () {
 var Trie = function () {
     this.root = new TrieNode();
 }
+
+Trie.prototype.insert = function (num) {
+    let node = this.root;
+    let nums = num.toString().split("");
+    while (nums.length > 0) {
+        let n = parseInt(nums.shift());
+        if (node.children[n] === undefined) {
+            node.children[n] = new TrieNode();
+        }
+        node = node.children[n];
+    }
+};
