0a8c1655f4ebdf389bc0b18c8aedaffd25c4e0ce
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 16:50:25 -0400

    Worked on Linked List Cycle

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,3 +1,15 @@
 var hasCycle = function (head) {
-
+    if (!head || !head.next) {
+        return false;
+    }
+    let slow = head.next;
+    let fast = slow.next;
+    while (slow !== fast) {
+        if (!fast) {
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next ? fast.next.next : null;
+    }
+    return true;
 };
