9fc8fdc9a526436090b76e0355b119ca1bf97137
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 16:29:11 -0400

    Worked on Happy Number

--- happyNumber.js
+++ happyNumber.js
@@ -1,3 +1,15 @@
 var isHappy = function (n) {
-
+    let slow = helper(n);
+    let fast = helper(slow);
+    if (fast === 1) {
+        return true;
+    }
+    while (slow !== fast) {
+        if (fast === 1) {
+            return true;
+        }
+        slow = helper(slow);
+        fast = helper(helper(fast));
+    }
+    return false;
 };
