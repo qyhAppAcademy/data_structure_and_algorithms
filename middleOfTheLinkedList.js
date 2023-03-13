d12efee0d406d7ae47477192238a3f62578d4652
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 17:03:01 -0400

    Worked on Linked List Cycle

--- middleOfTheLinkedList.js
+++ middleOfTheLinkedList.js
@@ -1,3 +1,12 @@
 var middleNode = function (head) {
-
+    if (!head || !head.next) {
+        return head;
+    }
+    let slow = head.next;
+    let fast = slow.next;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
 };
