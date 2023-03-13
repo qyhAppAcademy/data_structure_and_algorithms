657d3c807bd6a2bf568ae3b7544b32c024cee155
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 01:56:28 -0400

    Worked on Longest Palindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,3 +1,21 @@
 var longestPalindrome = function (s) {
-
+    const hashMap = {};
+    for (let i = 0; i < s.length; i++) {
+        if (hashMap[s[i]] === undefined) {
+            hashMap[s[i]] = 0;
+        }
+        hashMap[s[i]] += 1;
+    }
+    let isOdd = false;
+    let count = 0;
+    for (let c of Object.keys(hashMap)) {
+        if (hashMap[c] % 2 === 0) {
+            count += hashMap[c];
+        }
+        else {
+            count += hashMap[c] - 1;
+            isOdd = true;
+        }
+    }
+    return isOdd ? count + 1 : count;
 };
