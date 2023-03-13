008b9b9cb4fadeaad2b67d2939e3f41a55594afa
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 03:11:28 -0400

    Worked on Reverse Words In A String

--- reverseWordsInAString.js
+++ reverseWordsInAString.js
@@ -1,3 +1,18 @@
 var reverseWords = function (s) {
-
+    let newS = [];
+    for (let word of s.trim().split(" ").map(word => word.trim())) {
+        if (word !== "") {
+            newS.push(word);
+        }
+    }
+    let left = 0;
+    let right = newS.length - 1;
+    while (left < right) {
+        let tmp = newS[left];
+        newS[left] = newS[right];
+        newS[right] = tmp;
+        left += 1;
+        right -= 1;
+    }
+    return newS.join(" ");
 };
