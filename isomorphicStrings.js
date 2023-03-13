40d0a5fd3adf0d532c3b6c6602f547343f4f18f1
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 01:38:10 -0400

    Worked on Isomorphic Strings

--- isomorphicStrings.js
+++ isomorphicStrings.js
@@ -1,3 +1,14 @@
 var isIsomorphic = function (s, t) {
-
+    const hashMap = {};
+    for (let i = 0; i < s.length; i++) {
+        if (hashMap[s[i]] === undefined) {
+            hashMap[s[i]] = t[i];
+        }
+        else {
+            if (hashMap[s[i]] !== t[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
 };
