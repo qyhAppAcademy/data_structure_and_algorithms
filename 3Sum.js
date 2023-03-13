b4089ed1cf08a736ff13c5b989906dd9afe76748
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 03:03:03 -0400

    Worked on 3 Sum

--- 3Sum.js
+++ 3Sum.js
@@ -1,3 +1,31 @@
 var threeSum = function (nums) {
-
+    nums.sort((a, b) => a - b);
+    const result = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            if (nums[i] + nums[j] + nums[k] === 0) {
+                result.push([nums[i], nums[j], nums[k]]);
+                j += 1;
+                k -= 1;
+                while (nums[j] === nums[j - 1]) {
+                    j += 1;
+                }
+                while (nums[k] === nums[k + 1]) {
+                    k -= 1;
+                }
+            }
+            else if (nums[i] + nums[j] + nums[k] < 0) {
+                j += 1;
+            }
+            else {
+                k -= 1;
+            }
+        }
+    }
+    return result;
 };
