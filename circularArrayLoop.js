641fafb2b482cd87605959a9e1a18fb39e080fbf
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 00:16:48 -0400

    Worked on Circular Array Loop

--- circularArrayLoop.js
+++ circularArrayLoop.js
@@ -1,52 +1,33 @@
 var circularArrayLoop = function (nums) {
-    let len = nums.length;
     for (let i = 0; i < nums.length; i++) {
-        let isPositive = nums[i] > 0 ? true : false;
-        let slow = mod(i + nums[i], len);
-        if (i === slow) {
-            continue;
-        }
-        let fast;
-        if (nums[slow] > 0 === isPositive) {
-            fast = slow + nums[slow];
-        }
-        else {
-            continue;
-        }
-        let found = true;
-        while (slow !== fast) {
-            if (nums[slow] > 0 === isPositive) {
-                slow = mod(slow + nums[slow], len);
-                if (i === slow) {
-                    found = false;
-                    break;
-                }
-                if (nums[fast] > 0 === isPositive) {
-                    let tmp = mod(fast + nums[fast], len);
-                    if (nums[tmp] > 0 === isPositive) {
-                        fast = mod(tmp + nums[tmp], len);
-                    }
-                    else {
-                        found = false;
-                        break;
-                    }
-                }
-                else {
-                    found = false;
-                    break;
-                }
-            }
-            else {
-                found = false;
-                break;
+        let direction = nums[i] > 0 ? true : false;
+        let slow = nextStep(nums, i, direction);
+        let fast = nextStep(nums, slow, direction);
+        while (slow != null && fast !== null && slow !== fast) {
+            slow = nextStep(nums, slow, direction);
+            fast = nextStep(nums, fast, direction);
+            if (fast !== null) {
+                fast = nextStep(nums, fast, direction);
             }
         }
-        if (found) {
+        if (slow != null && fast !== null && slow === fast) {
             return true;
         }
     }
     return false;
 };
+
+var nextStep = function (nums, currentIndex, direction) {
+    let nextDirection = nums[currentIndex] > 0;
+    if (nextDirection !== direction) {
+        return null;
+    }
+    let nextIndex = mod(currentIndex + nums[currentIndex], nums.length);
+    if (nextIndex === currentIndex) {
+        return null;
+    }
+    return nextIndex;
+}
 
 var mod = function (n, m) {
     return ((n % m) + m) % m;
