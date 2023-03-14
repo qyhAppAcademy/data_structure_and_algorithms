ca8168d24cce5afa82763d190d38fb6b22fdd03c
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 12:55:50 -0400

    Worked on PalindromePermutation

--- palindromePermutation.js
+++ palindromePermutation.js
@@ -5,5 +5,21 @@ export class Solution {
      */
     canPermutePalindrome(s) {
         // write your code here
+        const hashMap = {};
+        for (let i = 0; i < s.length; i++) {
+            hashMap[s[i]] = hashMap[s[i]] === undefined ? 1 : hashMap[s[i]] + 1;
+        }
+        let hasOdd = false;
+        for (let c of Object.keys(hashMap)) {
+            if (hashMap[c] % 2 !== 0) {
+                if (hasOdd) {
+                    return false;
+                }
+                else {
+                    hasOdd = true;
+                }
+            }
+        }
+        return true;
     }
 }
