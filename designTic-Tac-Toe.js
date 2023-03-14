c8a93227684621ec2f255d171b008a3ffc4484f4
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 17:50:03 -0400

    Worked on Design Tic-Tac-Toe

---
+++ designTic-Tac-Toe.js
@@ -0,0 +1,15 @@
+class TicTacToe {
+    // Constructor will be used to initialize TicTacToe data members 
+    constructor(n) {
+
+    }
+
+    // move will be used to play a move by a specific player and identify who
+    // wins at each move
+    move(row, col, player) {
+        // your code will replace this placeholder return statement
+
+    }
+}
+
+export default TicTacToe
