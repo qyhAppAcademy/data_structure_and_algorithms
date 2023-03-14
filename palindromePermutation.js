1044f52e2a78d9897becc3e15813f9c427e03a0f
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 12:55:42 -0400

    Worked on PalindromePermutation

---
+++ palindromePermutation.js
@@ -0,0 +1,9 @@
+export class Solution {
+    /**
+     * @param s: the given string
+     * @return: if a permutation of the string could form a palindrome
+     */
+    canPermutePalindrome(s) {
+        // write your code here
+    }
+}
