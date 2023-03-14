aab0ee330981b3410f4d7ed116a3bb0b46fbe441
Qiao Yang Han <qyhwork@gmail.com>
2023-03-13 23:00:07 -0400

    Worked on Circular Array Loop

--- circularArrayLoop.js
+++ circularArrayLoop.js
@@ -1,5 +1,27 @@
 var circularArrayLoop = function (nums) {
-
+    if (nums.length <= 1) {
+        return false;
+    }
+    let len = nums.length;
+    let slow = nums[0];
+    let fast = mod(slow + nums[slow], len);
+    while (slow < nums.length) {
+        let newSlow = mod(slow + nums[slow], len);
+        let newFast = mod(fast + nums[fast], len);
+        if (slow === newSlow || fast === newFast) {
+            slow += 1;
+            slow = mod(slow + nums[slow], len);
+            fast = mod(slow + nums[slow], len);
+        }
+        else {
+            slow = newSlow;
+            fast = mod(newFast + nums[newFast], len);
+            if (slow === fast) {
+                return true;
+            }
+        }
+    }
+    return false;
 };
 
 var mod = function (n, m) {
