3827b47325cdd719e48510736c9fe1b54934a29d
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 23:00:58 -0400

    Worked on Design Tic-Tac-Toe

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,3 +1,11 @@
 var groupAnagrams = function (strs) {
-
+    const hashMap = {};
+    for (let str of strs) {
+        let sortedStr = str.split("").sort().join();
+        if (hashMap[sortedStr] === undefined) {
+            hashMap[sortedStr] = [];
+        }
+        hashMap[sortedStr].push(str);
+    }
+    return Object.values(hashMap);
 };
