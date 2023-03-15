7e326d65468a5de36166e6af8bdcbe824e6d4427
Qiao Yang Han <qyhwork@gmail.com>
2023-03-15 00:41:43 -0400

    Worked on Find All Anagrams In A String

--- findAllAnagramsInAString.js
+++ findAllAnagramsInAString.js
@@ -31,3 +31,15 @@ var findAnagrams = function (s, p) {
     }
     return result;
 };
+
+var equal = (obj1, obj2) => {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return false;
+    }
+    for (let key of Object.keys(obj1)) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    return true;
+}
