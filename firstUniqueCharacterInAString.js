2cbdc8d62b1c97ff8fef1f212772232840df731d
Qiao Yang Han <qyhwork@gmail.com>
2023-03-14 23:45:55 -0400

    Worked on First Unique Character In A String

--- firstUniqueCharacterInAString.js
+++ firstUniqueCharacterInAString.js
@@ -1,3 +1,17 @@
 var firstUniqChar = function (s) {
-
+    const hashMap = {};
+    for (let i = 0; i < s.length; i++) {
+        if (hashMap[s[i]] === undefined) {
+            hashMap[s[i]] = [];
+        }
+        hashMap[s[i]].push(i);
+    }
+    let min = undefined;
+    for (let key of Object.keys(hashMap)) {
+        if (hashMap[key].length === 1) {
+            let idx = hashMap[key][0];
+            min = min === undefined ? idx : Math.min(min, idx);
+        }
+    }
+    return min === undefined ? -1 : min;
 };
