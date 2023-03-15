af238b4a7daa854842fe60edd4af98f333a2371a
Qiao Yang Han <qyhwork@gmail.com>
2023-03-15 00:48:21 -0400

    Worked on Ransom Note

--- ransomNote.js
+++ ransomNote.js
@@ -1,2 +1,21 @@
 var canConstruct = function (ransomNote, magazine) {
+    const hashMap = {};
+    for (let i = 0; i < magazine.length; i++) {
+        let c = magazine[i];
+        if (hashMap[c] === undefined) {
+            hashMap[c] = 0;
+        }
+        hashMap[c] += 1;
+    }
+    for (let i = 0; i < ransomNote.length; i++) {
+        let c = ransomNote[i];
+        if (hashMap[c] === undefined) {
+            return false;
+        }
+        hashMap[c] -= 1;
+        if (hashMap[c] < 0) {
+            return false;
+        }
+    }
+    return true;
 };
