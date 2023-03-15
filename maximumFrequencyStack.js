f96c45c7288fb21276f84315430aa91d3ca970bd
Qiao Yang Han <qyhwork@gmail.com>
2023-03-15 01:30:56 -0400

    Worked on Maximum Frequency Stack

--- maximumFrequencyStack.js
+++ maximumFrequencyStack.js
@@ -1,5 +1,7 @@
 var FreqStack = function () {
-
+    this.frequency = {};
+    this.group = {};
+    this.maxFrequency = 0;
 };
 
 /** 
@@ -7,14 +9,30 @@ var FreqStack = function () {
  * @return {void}
  */
 FreqStack.prototype.push = function (val) {
-
+    this.frequency[val] = this.frequency[val] === undefined ? 1 : this.frequency[val] + 1;
+    if (this.group[this.frequency[val]] === undefined) {
+        this.group[this.frequency[val]] = [];
+    }
+    this.group[this.frequency[val]].push(val);
+    this.maxFrequency = Math.max(this.maxFrequency, this.frequency[val]);
 };
 
 /**
  * @return {number}
  */
 FreqStack.prototype.pop = function () {
-
+    if (this.maxFrequency === 0) {
+        return -1;
+    }
+    let val = this.group[this.maxFrequency].pop();
+    this.frequency[val] -= 1;
+    if (this.frequency[val] === 0) {
+        delete this.frequency[val];
+    }
+    if (this.group[this.maxFrequency].length === 0) {
+        this.maxFrequency -= 1;
+    }
+    return val;
 };
 
 /** 
