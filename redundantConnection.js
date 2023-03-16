6e3faddd36bfecca4e717cd028981861e5a1048a
Qiao Yang Han <qyhwork@gmail.com>
2023-03-15 23:55:51 -0400

    Worked on Find Redundant Connection

--- redundantConnection.js
+++ redundantConnection.js
@@ -1,2 +1,10 @@
 var findRedundantConnection = function (edges) {
+    let graph = new UnionFind(edges.length);
+    for (let i = 0; i < edges.length; i++) {
+        let v1 = edges[i][0];
+        let v2 = edges[i][1];
+        if (!graph.union(v1, v2)) {
+            return edges[i];
+        }
+    }
 };
