8e32f14e0f0e9341935d5d2e5f3c20069fe09cdb
Qiao Yang Han <qyhwork@gmail.com>
2023-03-15 23:56:00 -0400

    Worked on Find Redundant Connection

--- redundantConnection.js
+++ redundantConnection.js
@@ -8,3 +8,38 @@ var findRedundantConnection = function (edges) {
         }
     }
 };
+
+class UnionFind {
+
+    constructor(N) {
+        // intializing parent and rank lists
+        this.parent = [];
+        this.rank = new Array(N + 1).fill(1);
+        for (let i = 0; i <= N; i++) {
+            this.parent.push(i);
+        }
+    }
+
+    find_parent(x) {
+        if (this.parent[x] != x) {
+            this.parent[x] = this.find_parent(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    union(v1, v2) {
+        let p1 = this.find_parent(v1);
+        let p2 = this.find_parent(v2);
+        if (p1 == p2) {
+            return false;
+        }
+        else if (this.rank[p1] > this.rank[p2]) {
+            this.parent[p2] = p1;
+            this.rank[p1] += this.rank[p2];
+        } else {
+            this.parent[p1] = p2;
+            this.rank[p2] += this.rank[p1];
+        }
+        return true
+    }
+}
