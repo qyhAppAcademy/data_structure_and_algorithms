c5c1cfa3cc9a6fd2e27d9470bfd8e9daffd5b4ff
Qiao Yang Han <qyhwork@gmail.com>
2023-03-16 01:39:44 -0400

    Worked on Number of Islands

--- numberOfIslands.js
+++ numberOfIslands.js
@@ -1,3 +1,22 @@
 var numIslands = function (grid) {
-
+    let m = grid.length;
+    let n = grid[0].length;
+    const uf = new UnionFind(grid);
+    const offsets = [
+        [0, 1],
+        [1, 0]
+    ];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === "1") {
+                for (let offset of offsets) {
+                    if (i + offset[0] < m && j + offset[1] < n && grid[i][j] === grid[i + offset[0]][j + offset[1]]) {
+                        uf.union(i * n + j, (i + offset[0]) * n + (j + offset[1]));
+                    }
+                }
+                grid[i][j] = "0";
+            }
+        }
+    }
+    return uf.count;
 };
