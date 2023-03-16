7b9de4c6398488eec3b0c3905d1bf8cdf1485fae
Qiao Yang Han <qyhwork@gmail.com>
2023-03-16 01:39:52 -0400

    Worked on Number of Islands

--- numberOfIslands.js
+++ numberOfIslands.js
@@ -20,3 +20,49 @@ var numIslands = function (grid) {
     }
     return uf.count;
 };
+
+class UnionFind {
+
+    constructor(grid) {
+        // intializing parent and rank lists
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.parent = [];
+        this.rank = new Array(this.m * this.n).fill(1);
+        this.count = 0;
+        for (let i = 0; i < this.m; i++) {
+            for (let j = 0; j < this.n; j++) {
+                if (grid[i][j] === "1") {
+                    this.parent.push(i * this.n + j);
+                    this.count += 1;
+                }
+                else {
+                    this.parent.push(-1);
+                }
+            }
+        }
+    }
+
+    find_parent(x) {
+        if (this.parent[x] != x) {
+            this.parent[x] = this.find_parent(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    union(v1, v2) {
+        let p1 = this.find_parent(v1);
+        let p2 = this.find_parent(v2);
+        if (p1 == p2) {
+            return;
+        }
+        else if (this.rank[p1] > this.rank[p2]) {
+            this.parent[p2] = p1;
+            this.rank[p1] += this.rank[p2];
+        } else {
+            this.parent[p1] = p2;
+            this.rank[p2] += this.rank[p1];
+        }
+        this.count -= 1;
+    }
+}
