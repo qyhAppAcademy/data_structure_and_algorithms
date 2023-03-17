7ed1e8c72cf31ff11e099442b6d51cc0fe402504
Qiao Yang Han <qyhwork@gmail.com>
2023-03-17 01:01:14 -0400

    Worked on Last Day Where You Can Still Cross

---
+++ lastDayWhereYouCanStillCross.js
@@ -0,0 +1,21 @@
+class UnionFind {
+    constructor(n) {
+        this.parent = [];
+        for (let i = 0; i < n; i++) {
+            this.parent.push(i);
+        }
+    }
+
+    find_parent(x) {
+        if (this.parent[x] != x) {
+            this.parent[x] = this.find_parent(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+
+    union(v1, v2) {
+        let p1 = this.find_parent(v1);
+        let p2 = this.find_parent(v2);
+        this.parent[p2] = p1;
+    }
+}
