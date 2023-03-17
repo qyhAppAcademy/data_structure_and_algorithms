b876762b2ae993b8513877668f9d95d8b86d6266
Qiao Yang Han <qyhwork@gmail.com>
2023-03-17 14:40:16 -0400

    Worked on Evaluate Division

--- evaluateDivision.js
+++ evaluateDivision.js
@@ -1,2 +1,14 @@
 var calcEquation = function (equations, values, queries) {
+    const uf = new UnionFind(equations, values);
+    const result = [];
+    for (let query of queries) {
+        let [x, y] = query;
+        if (uf.parent[x] === undefined || uf.parent[y] === undefined || uf.find(x) !== uf.find(y)) {
+            result.push(-1.0);
+        }
+        else {
+            result.push(uf.weight[x] / uf.weight[y]);
+        }
+    }
+    return result;
 };
