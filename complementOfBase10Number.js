96dda792eb8e702957947fcc26fa207381785808
Qiao Yang Han <qyhwork@gmail.com>
2023-03-19 00:28:28 -0400

    Worked on Complement Of Base 10 Number

--- complementOfBase10Number.js
+++ complementOfBase10Number.js
@@ -1,2 +1,15 @@
 var bitwiseComplement = function (num) {
+    // If the value of num is 0, return 1
+    if (num == 0) {
+        return 1;
+    }
+    // Converting the value into its binary representation and
+    // counting the number of bits required by this number
+    let bitCount = Math.floor(Math.log2(num)) + 1
+
+    // Computing the all bits set of the number
+    let allBitsSet = Math.pow(2, bitCount) - 1
+
+    // Flipping all bits of number by taking XOR with allBitsSet
+    return num ^ allBitsSet
 };
