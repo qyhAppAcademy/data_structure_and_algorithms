5670c9ca80b3552c838ad359fcc51cc0fdca16be
Qiao Yang Han <qyhwork@gmail.com>
2023-03-19 00:33:50 -0400

    Worked on Complement Of Base 10 Number

--- flippingAnImage.js
+++ flippingAnImage.js
@@ -1,3 +1,14 @@
 var flipAndInvertImage = function (image) {
+    for (let i = 0; i < image.length; i++) {
+        image[i] = image[i].reverse()
+    }
 
+    for (let i = 0; i < image.length; i++) {
+        // Changing 1 to 0 and 0 to 1 within the row
+        for (let j in image[i]) {
+            image[i][j] = image[i][j] ^ 1
+        }
+    }
+
+    return image
 };
