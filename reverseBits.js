aaaae08f6403c1dbdcd667ac073f223acca8c78f
Qiao Yang Han <qyhwork@gmail.com>
2023-03-19 00:56:03 -0400

    Worked on Reverse Bits

--- reverseBits.js
+++ reverseBits.js
@@ -1,3 +1,6 @@
 var reverseBits = function (n) {
-
+    let reversed = n.toString(2);
+    const padding = "0";
+    reversed = padding.repeat(32 - reversed.length) + reversed;
+    return parseInt(reversed.split('').reverse().join(''), 2);
 };
