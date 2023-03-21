377bb2df83532fcd5dc4fab65663dcdcc9ef1e89
Qiao Yang Han <qyhwork@gmail.com>
2023-03-21 13:01:48 -0400

    Worked on Snapshot Array

---
+++ snapshotArray.js
@@ -0,0 +1,22 @@
+class SnapshotArray {
+    // Constructor
+    constructor(length) {
+
+    }
+
+    // Function setValue sets the value at a given index idx to val.
+    setValue(idx, val) {
+
+    }
+
+    // This function takes no parameters and returns the snapID.
+    // snapID is the number of times that the snapshot() function was called minus 1.
+    snapshot() {
+
+    }
+
+    // Function getValue returns the value at the index idx with the given snapID.
+    getValue(idx, snapID) {
+
+    }
+}
