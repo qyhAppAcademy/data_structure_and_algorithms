e7c8d4bab0322e4bbd5a8c804c6eb968b259713a
Qiao Yang Han <qyhwork@gmail.com>
2023-03-22 11:29:00 -0400

    Worked on Time Based Key Value Store

--- timeBasedKeyValueStore.js
+++ timeBasedKeyValueStore.js
@@ -3,7 +3,13 @@ var TimeMap = function () {
 };
 
 TimeMap.prototype.set = function (key, value, timestamp) {
+    // If the 'key' does not exist in dictionary.
+    if (!(key in keyTimeMap)) {
+        keyTimeMap[key] = {};
+    }
 
+    // Store '(timestamp, value)' pair in 'key' bucket.
+    keyTimeMap[key][timestamp] = value;
 };
 
 TimeMap.prototype.get = function (key, timestamp) {
