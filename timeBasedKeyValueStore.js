e114d24cbecf576173ce7198d7a856dbae5397b2
Qiao Yang Han <qyhwork@gmail.com>
2023-03-22 11:29:06 -0400

    Worked on Time Based Key Value Store

--- timeBasedKeyValueStore.js
+++ timeBasedKeyValueStore.js
@@ -13,5 +13,19 @@ TimeMap.prototype.set = function (key, value, timestamp) {
 };
 
 TimeMap.prototype.get = function (key, timestamp) {
+    // If the 'key' does not exist in dictionary we will return empty string.
+    if (!(key in keyTimeMap)) {
+        return "";
+    }
 
+    // Iterate on time from 'timestamp' to '1'.
+    for (let currTime = timestamp; currTime >= 1; --currTime) {
+        // If a value for current time is stored in key's bucket we return the value.
+        if (currTime in keyTimeMap[key]) {
+            return keyTimeMap[key][currTime];
+        }
+    }
+
+    // Otherwise no time <= timestamp was stored in key's bucket.
+    return "";
 };
