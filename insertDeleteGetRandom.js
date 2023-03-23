0a3e480a3a95e1dcbea65847e15deb3680104201
Qiao Yang Han <qyhwork@gmail.com>
2023-03-23 17:10:37 -0400

    Worked on Insert Delete Get Random

--- insertDeleteGetRandom.js
+++ insertDeleteGetRandom.js
@@ -1,6 +1,6 @@
 var RandomizedSet = function () {
-    this.indexor = {};
-    this.store = [];
+    this.indexor = {}; 
+    this.store = []; 
 };
 
 /** 
@@ -8,7 +8,17 @@ var RandomizedSet = function () {
  * @return {boolean}
  */
 RandomizedSet.prototype.insert = function (val) {
+    //Inserts a value in the data structure.
+    //Returns True if it did not already contain the specified element.
 
+    if (val in this.indexor) {
+        return false;
+    }
+    // insert actual value as a key and its index as a value
+    this.indexor[val] = this.store.length;
+    // append new value to array
+    this.store.push(val);
+    return true;
 };
 
 /** 
