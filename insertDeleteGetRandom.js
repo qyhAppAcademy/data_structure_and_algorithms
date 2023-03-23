e8b030372f6f6cf192be4bc890bfb88bb0506339
Qiao Yang Han <qyhwork@gmail.com>
2023-03-23 17:09:58 -0400

    Worked on Insert Delete Get Random

---
+++ insertDeleteGetRandom.js
@@ -0,0 +1,25 @@
+var RandomizedSet = function () {
+
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+};
