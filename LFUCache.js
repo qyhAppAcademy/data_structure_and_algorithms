6ac79b55a0b02d0ea3bfbaaef06fe96cf5495650
Qiao Yang Han <qyhwork@gmail.com>
2023-03-24 16:07:27 -0400

    Worked on LFU Cache

--- LFUCache.js
+++ LFUCache.js
@@ -6,3 +6,36 @@ class Node {
         this.freq = 1;
     }
 }
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = new Node(null, null);
+        this.tail = new Node(null, null);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    insertHead(node) {
+        node.prev = this.head;
+        node.next = this.head.next;
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
+
+    removeNode(node) {
+        let prev = node.prev;
+        let next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    removeTail() {
+        let node = this.tail.prev;
+        this.removeNode(node);
+        return node.key;
+    }
+
+    isEmpty() {
+        return this.head.next.val == null;
+    }
+}
